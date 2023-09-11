cb432ab027823bb80aea494f077efeed4bc8500b
dengzhu-hub <3052113768@qq.com>
2023-09-11 14:18:16 +0800

    use ReactComponent to use svg

--- src/routes/navigation/navigation.component.jsx
+++ src/routes/navigation/navigation.component.jsx
@@ -1,12 +1,14 @@
 import { Fragment } from "react";
 import { Outlet, Link } from "react-router-dom";
+import { ReactComponent as CrowLogo } from "../../assets/crown.svg";
 function Navigation() {
   return (
     <Fragment>
       <div className="nav">
-        <lINK className="nav-logo__container" to="/">
-          <div className="nav-logo">LOGO</div>
-        </lINK>
+        <Link className="nav-logo__container" to="/">
+          <CrowLogo className="nav-logo" />
+          
+        </Link>
         <div className="nav-link__container">
           <Link className="nav-link__to" to="/shop">
             SHOP
