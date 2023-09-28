989be26f98f1a2646bff1bac954dafebe04041b5
dengzhu-hub <3052113768@qq.com>
2023-09-28 12:21:40 +0800

    创建category-preview模块化，让代码更加可读性

--- src/components/category-preview/category-preview.component.jsx
+++ src/components/category-preview/category-preview.component.jsx
@@ -1,10 +1,22 @@
-import './category-preview.style.scss';
-import React from 'react'
+import ProductCard from "../product-card/product-card.component";
+import "./category-preview.style.scss";
+import React from "react";
 
-const CategoryPreview = () => {
+const CategoryPreview = ({ title, products }) => {
   return (
-    <div>CategoryPreview</div>
-  )
-}
+    <div className="category-preview__container">
+      <h2 className="title">
+        <span>{title.toUpperCase()}</span>
+      </h2>
+      <div className="preview">
+        {products
+          .filter((_, index) => index < 4)
+          .map(product => (
+            <ProductCard key={product.id} product={product} />
+          ))}
+      </div>
+    </div>
+  );
+};
 
-export default CategoryPreview
+export default CategoryPreview;
--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,22 +1,18 @@
-import { Fragment, useContext } from "react";
+import { useContext } from "react";
 import "./shop.style.scss";
 import { ProductsContext } from "../../contexts/products.context";
-import ProductCard from "../../components/product-card/product-card.component";
+import CategoryPreview from "../../components/category-preview/category-preview.component";
 const Shop = () => {
   const { categoriesMap } = useContext(ProductsContext);
   return (
-    <Fragment>
-      {Object.keys(categoriesMap).map(title => (
-        <Fragment key={title}>
-          <h2 className="category-title">{title}</h2>
-          <div className="products-card__container">
-            {categoriesMap[title].map(product => (
-              <ProductCard key={product.id} product={product}></ProductCard>
-            ))}
-          </div>
-        </Fragment>
-      ))}
-    </Fragment>
+    <div className="shop-container">
+      {Object.keys(categoriesMap).map(title => {
+        const products = categoriesMap[title];
+        return (
+          <CategoryPreview key={title} title={title} products={products} />
+        );
+      })}
+    </div>
   );
 };
 export default Shop;
